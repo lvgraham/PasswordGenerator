552d7246a4f30c5476a5bc65b827a9eb5cdfec44
lvgraham <liavibanez@gmail.com>
2020-05-04 23:59:24 -0500

    adding starter files & listing functions

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+let 
+
+function hasNumbers() {}
+function hasUppercase() {}
+function hasLowercase() {}
+function hasSpecialCharacters() {}
+function getNumber() {}
+function getLowercase() {}
+
+
+function generatePassword() {}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
